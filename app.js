8487e5c50b1eaff454d2c10aebdd26ed378c27d5
kbsceo <leedw__v@naver.com>
2015-08-14 03:12:04 +0900

    RESTfulAPI : add exception 404, 500 error code, mongodb connection code

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var logger = require('morgan');
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var wines = require('./routes/wines');
 
 
 var app = express();
@@ -17,16 +18,54 @@ app.use(logger('dev'));
 app.use('/', routes);
 app.use('/users', users);
 
-// app listen
+app.settings.env(function (){
+    app.use(express.logger('dev'));
+    app.use(express.bodyPaser());
+})
 
 app.get('/api', function (req, res) {
     res.send('API is running');
 });
+app.get('/wines', wines.findAll);
+app.get('/wines/:id', wines.findById());
+app.post('/wines', wines.addWine);
+app.put('/wines/:id', wines.updateWine);
+app.delete('/wines/:id', wines.deleteWine);
+
 
 app.listen(app.get('port'), function () {
     console.log('Server is running at 3030');
 });
 
 
+process.on('uncaughtException', function (err) {
+    console.log('CaughtException' + err);
+});
+
+app.use(function (req, res, next) {
+    res.staus(404);
+
+});
+// request
+if(req.accept('html')) {
+    res.render('404', { url: req.url});
+    return;
+}
+
+// response
+if(res.accept('json')) {
+    res.send({ error: 'Not Found'});
+    return;
+}
+
+// default to plain-text
+res.type('txt').send('Not Found');
+
 
 module.exports = app;
+
+
+
+
+
+
---
+++ routes/wine.js
@@ -0,0 +1,28 @@
+/**
+ * Created by leedw on 15. 8. 14..
+ */
+
+
+var mongo = require('mongodb');
+
+var Server = mongo.Server;
+var Db = mongo.Db;
+var BSON = mongo.BSONPure;
+
+exports.findAll = function(req, res) {
+    res.send([{name : 'wine1'}, {name : 'wine2'}, {name:'wine3'}]);
+};
+
+exports.findById = function(req, res) {
+    res.send({id:req.params.id, name: "The name", description: "description"});
+}
+
+var server = new Server('localhost', 3030, {auto_reconnect: true});
+db = new Db('winedb', server);
+
+db.open(function(err, db){
+    if(err) {
+        console.log('The wines collection doesnt exist');
+        populateDB();
+    }
+})
